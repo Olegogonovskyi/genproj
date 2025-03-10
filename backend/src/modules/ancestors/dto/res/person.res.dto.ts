4e86262d77ff85a9363ac66d273b62e112ff6a98
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-03-10 22:00:34 +0200

    10_03: add ancestorModule

---
+++ backend/src/modules/ancestors/dto/res/person.res.dto.ts
@@ -0,0 +1,60 @@
+import { Exclude, Type } from 'class-transformer';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { FamilyResDto } from './family.res.dto';
+
+export class PersonResDto {
+  @IsOptional()
+  @IsString()
+  id?: string;
+
+  @IsString()
+  insideId: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  surName?: string;
+
+  @IsOptional()
+  @IsString()
+  marriedSurName?: string;
+
+  @IsString()
+  sex: string;
+
+  @IsBoolean()
+  isDead?: boolean;
+
+  @IsOptional()
+  @IsString()
+  npfx?: string;
+
+  @IsOptional()
+  @IsString()
+  note?: string;
+
+  @IsOptional()
+  @IsString()
+  object?: string;
+
+  @Exclude()
+  @Type(() => FamilyResDto)
+  @IsOptional()
+  familyAsParent?: FamilyResDto;
+
+  @Exclude()
+  @Type(() => FamilyResDto)
+  @IsOptional()
+  familyAsChild: FamilyResDto;
+
+  @IsOptional()
+  @IsString()
+  birth: string;
+
+  @IsOptional()
+  @IsString()
+  death: string;
+}
---
+++ backend/src/modules/ancestors/dto/res/singlePerson.res.dto.ts
@@ -0,0 +1,18 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class SinglePersonResDto {
+  @IsOptional()
+  @IsString()
+  id?: string;
+
+  @IsString()
+  insideId: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  surName?: string;
+}
