54ee5b6b1793d2c2630e1da2347387d545d619c6
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-03-26 14:09:49 +0200

    26_03: play with redux

--- client/src/services/articles.api.service.tsx
+++ client/src/services/articles.api.service.tsx
@@ -2,6 +2,7 @@ import {axiosInstanse} from "./axios.api.service";
 import { articleUrls } from '../costants/Urls';
 import {IPaginationModel} from "../Models/IPaginationModel";
 import { IArticleReqModel } from '../models/IArticleReqModel';
+import { ISearchServiceType } from '../models/ISearchServiceType';
 
 export const articlesApiService = {
     getAllArticles: async (page: string): Promise<IPaginationModel<IArticleReqModel>> => {
@@ -9,8 +10,8 @@ export const articlesApiService = {
         return data
     },
 
-    getAllArticles: async (page: string): Promise<IPaginationModel<IArticleReqModel>> => {
-        const {data} = await axiosInstanse.get<IPaginationModel<IArticleReqModel>>(articleUrls.getAllArticles, {params: {page: page}})
+    searchArticles: async ({page, query: {query}  }: ISearchServiceType): Promise<IPaginationModel<IArticleReqModel>> => {
+        const {data} = await axiosInstanse.get<IPaginationModel<IArticleReqModel>>(articleUrls.getAllArticles, {params: {page: page, }})
         return data
     }
 }
