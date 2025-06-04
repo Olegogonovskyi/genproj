51a45c7462017f5f19d98af17cc8b010c4312726
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-06-04 15:12:06 +0300

    04_06: add  timeline

--- backend/src/helpers/transform/parseCustomDateOne.ts
+++ backend/src/helpers/transform/parseCustomDateOne.ts
@@ -3,24 +3,24 @@ import { months } from '../costants/fieldconstants';
 
 @Injectable()
 export class ParseCustomDateOne {
-  public static stringToDate(dateString: string): Date {
+  public static stringToDate(dateString: string): string {
     const parts = dateString.split(' ');
 
     if (parts.length === 3) {
       const [day, month, year] = parts;
       const monthIndex = this.getMonthIndex(month);
       return monthIndex !== null
-        ? new Date(Number(year), monthIndex, Number(day))
-        : new Date(Number(year), 0); // Якщо місяць невідомий
+        ? toString((year), monthIndex, Number(day))
+        : toString(Number(year), 0); // Якщо місяць невідомий
     } else if (parts.length === 2) {
       const [month, year] = parts;
       const monthIndex = this.getMonthIndex(month);
       return monthIndex !== null
-        ? new Date(Number(year), monthIndex)
-        : new Date(Number(year), 0);
+        ? toString(Number(year), monthIndex)
+        : toString(Number(year), 0);
     } else if (parts.length === 1) {
       const [year] = parts;
-      return new Date(Number(year));
+      return year
     }
   }
 
