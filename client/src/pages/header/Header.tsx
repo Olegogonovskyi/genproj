8162ca5d594e05b632c3bd6d8797276360f8b7c1
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-06-11 11:07:50 +0300

    09_06: add css Header.tsx

--- client/src/pages/header/Header.tsx
+++ client/src/pages/header/Header.tsx
@@ -2,52 +2,57 @@ import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import SearchFormComponent from '../../components/SearchFormComponent/SearchFormComponent';
 import { apiUrls, baseUrls } from '../../costants/Urls';
+import style from './Header.module.css'
+import { styled } from '@mui/material';
 
 const Header: FC = () => {
   return (
     <div>
-      <SearchFormComponent />
-      <nav>
-        <ul>
-          {/* Auth */}
-          <li><NavLink to={apiUrls.auth.register}>Register</NavLink></li>
-          <li><NavLink to={apiUrls.auth.login}>Login</NavLink></li>
 
-          <hr />
+      <div className={style.header}>
+        <SearchFormComponent />
+        <div className={style.nav}>
+          <ul>
+            {/* Auth */}
+            <li><NavLink to={apiUrls.auth.register}>Register</NavLink></li>
+            <li><NavLink to={apiUrls.auth.login}>Login</NavLink></li>
 
-          {/* Ancestors */}
-          <li><NavLink to={apiUrls.ancestors.getAllAncestors}>All Ancestors</NavLink></li>
-          <li><NavLink to={apiUrls.ancestors.getAllAncestorsDates}>All Ancestors Dates</NavLink></li>
+            <hr />
 
-          <hr />
+            {/* Ancestors */}
+            <li><NavLink to={apiUrls.ancestors.getAllAncestors}>All Ancestors</NavLink></li>
+            <li><NavLink to={apiUrls.ancestors.getAllAncestorsDates}>All Ancestors Dates</NavLink></li>
 
-          {/* Articles */}
-          <li><NavLink to={apiUrls.article.getAll}>All Articles</NavLink></li>
-          <li><NavLink to={apiUrls.article.create}>Create Article</NavLink></li>
+            <hr />
 
-          <hr />
+            {/* Articles */}
+            <li><NavLink to={apiUrls.article.getAll}>All Articles</NavLink></li>
+            <li><NavLink to={apiUrls.article.create}>Create Article</NavLink></li>
 
-          {/* Chronology */}
-          <li><NavLink to={apiUrls.chronology.create}>Create Date</NavLink></li>
-          <li><NavLink to={baseUrls.chronology}>All Dates</NavLink></li>
+            <hr />
 
-          <hr />
+            {/* Chronology */}
+            <li><NavLink to={apiUrls.chronology.create}>Create Date</NavLink></li>
+            <li><NavLink to={baseUrls.chronology}>All Dates</NavLink></li>
 
-          {/* GED File */}
-          <li><NavLink to={apiUrls.uploadGed.upload}>Upload GED</NavLink></li>
+            <hr />
 
-          <hr/>
+            {/* GED File */}
+            <li><NavLink to={apiUrls.uploadGed.upload}>Upload GED</NavLink></li>
 
-          {/* users simple */}
-          <li><NavLink to={apiUrls.users.me}>Get Me</NavLink></li>
+            <hr/>
 
-          <hr/>
+            {/* users simple */}
+            <li><NavLink to={apiUrls.users.me}>Get Me</NavLink></li>
 
-          {/* users byAdmin */}
-          <li><NavLink to={apiUrls.users.create}>create User By Admin</NavLink></li>
-          <li><NavLink to={baseUrls.adminUsers}>list of users</NavLink></li>
-        </ul>
-      </nav>
+            <hr/>
+
+            {/* users byAdmin */}
+            <li><NavLink to={apiUrls.users.create}>create User By Admin</NavLink></li>
+            <li><NavLink to={baseUrls.adminUsers}>list of users</NavLink></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
