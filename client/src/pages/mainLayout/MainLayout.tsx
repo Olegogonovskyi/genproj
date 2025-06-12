8ef028e35c94ed79020b114f6c14a2bf480cc9fc
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-06-12 13:08:15 +0300

    09_06: add css Header.tsx

--- client/src/pages/mainLayout/MainLayout.tsx
+++ client/src/pages/mainLayout/MainLayout.tsx
@@ -1,17 +1,16 @@
 import React, {FC} from 'react';
 import Header from "../header/Header";
 import {Outlet} from "react-router-dom";
-import Footer from "../footer/Footer";
+import style from './MainLayout.module.css'
 
 const MainLayout: FC = () => {
 
     return (
         <div>
-            <Header/>
-          <h1>test</h1>
-            <Outlet/>
-            <hr/>
-            <Footer/>
+            <div className={style.mainContainer}>
+              <Header/>
+              <Outlet/>
+            </div>
         </div>
     );
 };
--- client/src/pages/mainPage/MainPage.tsx
+++ client/src/pages/mainPage/MainPage.tsx
@@ -1,9 +1,32 @@
-import React, {FC} from 'react';
+
+import React, {FC, useState } from 'react';
+import style from './MainPage.module.css'
+import imageSrc from '../../images/14-03_Jaremvevych_F_027-Colorized-Photoroom.png';
+import StepperComponent from '../../components/stepperComponent/StepperComponent';
+
 
 const MainPage: FC = () => {
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const maxSteps = 2;
   return (
     <div>
-      mainPage
+      {activeStep === 0 && <section className={style.pageOne}>
+        <div className={style.textBox}>
+          <h1>Родинний сайт Огоновських-Пахолюків</h1>
+          <p>
+            Тут описано генеалогію предків Олега Огоновського (Огоновські, Сколоздри, Корецькі, Скрипці, Ядвіжаки, Музики та багато інших) в основному з таких локацій: Розвадів, Дроговиж, Верин
+            Та генеалогію предків Олени Огоновської (Пахолюк) (Пахолюки, Мурини, Рибіцькі, Ковбасюки, Боднар, Яремкевичі, Подолинські, Гречники, Венгери, Лазоришаки, Чайківські ) в основному з таких локацій: Вовків, Пустомити, Заланів, Петрів, Незвиська та ін.
+
+          </p>
+        </div>
+        <div className={style.round}>
+          <img className={style.mainImage} src={imageSrc} alt='genealogyWebSite_Podolynskyi'/>
+        </div>
+      </section>}
+      {activeStep === 1 && <section className={style.pageTwo}> ... </section>}
+      <div className={style.stepperComponent}>
+        <StepperComponent  steps={maxSteps} activeStep={activeStep} setStep={setActiveStep} maxSteps={maxSteps} />
+      </div>
     </div>
   );
 };
