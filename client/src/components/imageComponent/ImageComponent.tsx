3c18d78cc6c354f61a086c46675863c55f3382ec
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-07-16 15:18:18 +0300

    16_07: images module

---
+++ client/src/components/imageComponent/ImageComponent.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+import style from './ImageComponent.module.css'
+import {UploadFotoApiService} from "../../services/uploadFoto.api.service";
+
+const ImageComponent: FC<{ imageUrl: string }> = ({imageUrl}) => {
+    const imageName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+    const keyToDell = imageUrl.split('/').slice(-2).join('/');
+
+    const copyLink = async () => {
+      try {
+          await navigator.clipboard.writeText(imageUrl);
+      } catch (error) {
+          console.log(error)
+      }
+    }
+
+    return (
+        <div className={style.wrap}>
+            <img src={imageUrl} alt={imageName}/>
+            <p>{imageName}</p>
+            <button onClick={() => copyLink()}>Копіювати лінку</button>
+            <button onClick={async () => await UploadFotoApiService.deleteImage(keyToDell)}>Копіювати лінку</button>
+        </div>
+    );
+};
+
+export default ImageComponent;
--- client/src/helpers/convertYoutubeUrlToEmbed.ts
+++
@@ -1,5 +0,0 @@
-export const convertYoutubeUrlToEmbed = (url: string) => {
-  const regex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^\s&]+)/;
-  const match = url.match(regex);
-  return match ? `https://www.youtube.com/embed/${match[1]}` : url;
-};
