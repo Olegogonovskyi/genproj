d094eed8d85118696d500c393f0fb025352cd12c
Oleg Ogonowskyi <olegogonowskyi@gmail.com>
2025-07-28 11:55:57 +0300

    28_07: work with tags on back and users

--- backend/src/modules/tag/tag.module.ts
+++ backend/src/modules/tag/tag.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ArticleModule } from '../articlesNew/article.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [JwtModule, ArticleModule, AuthModule],
   controllers: [TagController],
   providers: [TagService],
 })
